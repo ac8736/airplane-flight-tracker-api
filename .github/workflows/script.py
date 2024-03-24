d4b1df2d38889d916862b7fbe595dbb4c99822cf
Kewl <87680132+ac8736@users.noreply.github.com>
2024-03-24 18:28:51 -0400

    update setup script

--- .github/workflows/script.py
+++ .github/workflows/script.py
@@ -20,15 +20,11 @@ cursor.execute(query)
 
 query = "CREATE TABLE airline(name varchar(255) not null, primary key (name));"
 cursor.execute(query)
-query = "INSERT INTO airline VALUES('United-Airlines');"
-cursor.execute(query)
 
 query = "CREATE TABLE airport(name varchar(255) not null, code varchar(255) not null, city varchar(255) not null, country varchar(255) not null, dateCreated Datetime not null);"
 cursor.execute(query)
 
 query = "CREATE TABLE airline_staff(username varchar(255) not null, email varchar(255) not null, acc_password varchar(255) not null, firstName varchar(255) not null, lastName varchar(255) not null, airline varchar(255) not null, dateCreated Datetime not null, lastModified Datetime not null);"
-cursor.execute(query)
-query = "INSERT INTO airline_staff VALUES('johndoe123', 'abc@email.com', '123', 'John', 'Doe', 'United-Airlines', '2024-03-23 12:34:56', '2024-03-23 12:34:56');"
 cursor.execute(query)
 
 conn.commit()
